f27736791f2a5f1485b7fc014f56d075b84249c9
narendradivi <narendraavula2@gmail.com>
2017-03-25 16:56:18 +0530

    country details - google maps

---
+++ app/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('angularMaps', [
+    'ui.router', 
+    'ui.bootstrap', 
+    'ui.select', 
+    'uiGmapgoogle-maps'
+]);
---
+++ app/controllers/MapCtrl.js
@@ -0,0 +1,52 @@
+app.controller('MapCtrl', ['$scope', '$state', 'Country', function ($scope, $state, Country) {
+
+    var mapCtrl = this,
+        countryName = $state.params.name,
+        selectedCountry = Country.selected,
+        latitude = null,
+        longitude = null;
+
+    /**
+     * Refreshes the map based upon the passed parameters
+     */
+    var refreshMap = function ( latitude, longitude, center ) {
+
+        center = center || 9;
+
+        mapCtrl.map = {
+            center: {
+                latitude: latitude, 
+                longitude: longitude 
+            },
+            zoom: center
+        };
+    };
+
+    // If the country is available
+    if ( selectedCountry ) {
+
+        latitude = selectedCountry.latlng[0];
+        longitude = selectedCountry.latlng[1];
+
+        refreshMap( latitude, longitude );
+
+    } else if( countryName ) {
+
+        Country.selectedName = countryName;
+
+        // Fetch the country;
+        Country.getDetail(countryName).then(function( countryDetail ) {
+            Country.selected = selectedCountry = countryDetail[0];        // Populate in the service so to reuse it in other controllers
+
+            latitude  = selectedCountry.latlng[0];
+            longitude = selectedCountry.latlng[1];
+
+            refreshMap(latitude, longitude);
+
+        }, function () {
+            console.error('Unknown error occured while trying to fetch the country.');
+        });
+
+    }
+
+}]);
